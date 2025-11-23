cd7ebc87955e85cd70277e57b7907d58a3ece11a
Liam Taccon <wengshinliam@gmail.com>
2025-11-23 21:30:16 +0100

    kk

--- PasDiscretes/TpNoteMatrices.py
+++ PasDiscretes/TpNoteMatrices.py
@@ -1,12 +1,89 @@
 import numpy as np
 import math as m
 
-A = np.array(([[(m.sqrt(6)+m.sqrt(2))*0.25,(m.sqrt(2)-m.sqrt(6))*0.25],[(m.sqrt(6)-m.sqrt(2))*0.25,(m.sqrt(6)+m.sqrt(2))*0.25]]))
+A = [[(m.sqrt(6)+m.sqrt(2))*0.25,(m.sqrt(2)-m.sqrt(6))*0.25],
+     [(m.sqrt(6)-m.sqrt(2))*0.25,(m.sqrt(6)+m.sqrt(2))*0.25]]
 B = A
-def Exo1(A,n):
-    B = A
-    for i in range(1,n):
-        B = B @ A
-        print(B)
+I = [[1,0],[0,1]]
 
-print(Exo1(B,24))
+
+def ProduitMatriciel(M,N):
+    return ([M[0][0]*N[0][0] + M[0][1]*N[1][0],
+            M[0][0]*N[0][1] + M[0][1]*N[1][1]],
+            [M[1][0]*N[0][0] + M[1][1]*N[1][0],
+             M[1][0]*N[0][1] + M[1][1]*N[1][1]])
+
+def TestIdentite(M):
+    tol = 0.00001
+    if (abs(M[0][0] - 1) < tol and abs(M[1][1] - 1) < tol and abs(M[0][1]) < tol and abs(M[1][0]) < tol):
+        return True
+
+
+def Exo1(A):
+    B = A
+    n = 1 #n = 1 car A = A^1
+    while not TestIdentite(B):
+        B = ProduitMatriciel(B,A)
+        n += 1
+
+    print("n =",n)
+
+def Exo5():
+    Mmax = [50,20,30,5]
+    Mactu = [35,20,10,2]
+    Poids = [0.1,0.2,0.3,1]
+    Puissance = [10,8,20,17]
+
+    PoidsMax = Mmax[0]*Poids[0]+Mmax[1]*Poids[1]+Mmax[2]*Poids[2]+Mmax[3]*Poids[3]
+    PoidsActu = Mactu[0]*Poids[0]+Mactu[1]*Poids[1]+Mactu[2]*Poids[2]+Mactu[3]*Poids[3]
+    PuissanceFeu = Mactu[0]*Puissance[0]+Mactu[1]*Puissance[1]+Mactu[2]*Puissance[2]+Mactu[3]*Puissance[3]
+
+
+
+    print("Poids Max =",PoidsMax,"Poids Actuel =",PoidsActu, "puissance de feu =",PuissanceFeu)
+
+
+
+def Exo6():
+    Stats = np.array([
+             [48.20,75,107,100,63,63,72,65],
+             [63.38,58,91,82,74,52,65,45],
+             [43.38,64,100,91,57,57,72,72],
+             [39.52,85,75,107,52,74,59,50]])         #j'ai divisé les pv par 100
+
+    classe = np.array([
+             [0.5, 0.1, 1.5, 0.1],  # PV
+             [0.1, 0.5, 0.1, 0.5],  # PM
+             [1.5, 0.1, 0.5, 0.1],  # Force
+             [0.1, 1.5, 0.1, 0.1],  # Magie
+             [1.0, 0.1, 1.5, 0.1],  # Constitution
+             [0.1, 1.0, 0.5, 0.1],  # Esprit
+             [0.1, 0.1, 0.1, 1.0],  # Chance
+             [0.1, 0.1, 0.1, 1.5]   # Rapidité
+    ])
+
+    perf = Stats @classe
+    print(perf)
+
+def Exo7():
+    M = np.array([[0,10,10,0],
+                  [0,0,10,10]
+                  ])
+
+    symetrie = np.array([[-1,0],
+                         [0,1]])
+    homothetie = np.array([[2,0],
+                           [0,2]])  #x2
+    rotation = np.array([[0,-1],
+                         [1,0]])    #pi/2
+
+    M =symetrie @ M
+    M =homothetie @ M
+    M =rotation @ M
+
+    return M
+
+print(Exo1(A))
+print(Exo5())
+print(Exo6())
+print(Exo7())
