e5e17fc73139207fd7a81732c0729f6bd312b128
Liam <ltconflans@gmail.com>
2025-11-26 15:53:59 +0100

    b

--- PasDiscretes/TpNoteMatrices.py
+++ PasDiscretes/TpNoteMatrices.py
@@ -1,5 +1,8 @@
+from symtable import Class
+
 import numpy as np
 import math as m
+import matplotlib.pyplot as plt
 
 A = [[(m.sqrt(6)+m.sqrt(2))*0.25,(m.sqrt(2)-m.sqrt(6))*0.25],
      [(m.sqrt(6)-m.sqrt(2))*0.25,(m.sqrt(6)+m.sqrt(2))*0.25]]
@@ -25,18 +28,17 @@ def Exo1(A):
     while not TestIdentite(B):
         B = ProduitMatriciel(B,A)
         n += 1
-
     print("n =",n)
 
 def Exo5():
-    Mmax = [50,20,30,5]
-    Mactu = [35,20,10,2]
-    Poids = [0.1,0.2,0.3,1]
-    Puissance = [10,8,20,17]
+    Mmax = np.array([50,20,30,5])
+    Mactu = np.array([35,20,10,2])
+    Poids = np.array([0.1,0.2,0.3,1])
+    Puissance = np.array([10,8,20,17])
 
-    PoidsMax = Mmax[0]*Poids[0]+Mmax[1]*Poids[1]+Mmax[2]*Poids[2]+Mmax[3]*Poids[3]
-    PoidsActu = Mactu[0]*Poids[0]+Mactu[1]*Poids[1]+Mactu[2]*Poids[2]+Mactu[3]*Poids[3]
-    PuissanceFeu = Mactu[0]*Puissance[0]+Mactu[1]*Puissance[1]+Mactu[2]*Puissance[2]+Mactu[3]*Puissance[3]
+    PoidsMax = Mmax.dot(Poids)
+    PoidsActu = Mactu.dot(Poids)
+    PuissanceFeu = Mactu.dot(Puissance)
 
 
 
@@ -52,38 +54,46 @@ def Exo6():
              [39.52,85,75,107,52,74,59,50]])         #j'ai divisé les pv par 100
 
     classe = np.array([
-             [0.5, 0.1, 1.5, 0.1],  # PV
-             [0.1, 0.5, 0.1, 0.5],  # PM
-             [1.5, 0.1, 0.5, 0.1],  # Force
-             [0.1, 1.5, 0.1, 0.1],  # Magie
-             [1.0, 0.1, 1.5, 0.1],  # Constitution
-             [0.1, 1.0, 0.5, 0.1],  # Esprit
-             [0.1, 0.1, 0.1, 1.0],  # Chance
-             [0.1, 0.1, 0.1, 1.5]   # Rapidité
+             [0,0,1,0],  # PV
+             [0,1,0,0],  # PM
+             [1,0,0,0],  # Force
+             [0,1,0,0],  # Magie
+             [0,0,1,0],  # Constitution
+             [1,0,0,0],  # Esprit
+             [0,0,0,1],  # Chance
+             [0,0,0,1]   # Rapidité
+      # guerrier / mage / tank /voleur
     ])
 
-    perf = Stats @classe
+    perf = Stats.dot(classe)
     print(perf)
 
 def Exo7():
-    M = np.array([[0,10,10,0],
-                  [0,0,10,10]
+    M = np.array([[0,10,10,0,0],
+                  [0,0,10,10,0]
                   ])
+    plt.plot(M[0],M[1])
+
 
     symetrie = np.array([[-1,0],
                          [0,1]])
     homothetie = np.array([[2,0],
-                           [0,2]])  #x2
+                           [0,2]])  #*2
     rotation = np.array([[0,-1],
                          [1,0]])    #pi/2
 
-    M =symetrie @ M
-    M =homothetie @ M
-    M =rotation @ M
+    M =symetrie.dot(M)
+    M =homothetie.dot(M)
+    M =rotation.dot(M)
+
+    plt.plot(M[0],M[1])
+    plt.show()
+
 
     return M
 
-print(Exo1(A))
-print(Exo5())
+#print(Exo1(A))
+#print(Exo5())
 print(Exo6())
-print(Exo7())
+#print(Exo7())
+
